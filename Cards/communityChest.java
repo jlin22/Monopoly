f7a03f59e744ff5f08d3a926b109bbb09ba9cff6
Alan Zhou <azhou2@stuy.edu>
2017-01-02 16:04:43 -0500

    just typed all of them out

---
+++ Cards/communityChest.java
@@ -0,0 +1,17 @@
+//CommunityChests
+
+"Collect $50 From Every Player."
+"Receive $25 For Services."
+"Advance To Go (Collect $200)."
+"Pay Hospital Fees. Pay $100."
+"Doctor's Fee. Pay $50."
+"Get Out of Jail, Free."
+"From Sale of Stock, You Get $45."
+"You Inherit $100."
+"Go To Jail. Go Directly to Jail. Do Not Pass Go. Do Not collect $200."
+"Life Insurance Matures. Collect $100."
+"You Have Won Second Prize In A Beauty Contest. Collect $10"
+"Xmas Fund Matures. Collect $100."
+"You Are Assessed For Street Repairs. $40 Per House. $115 Per Hotel."
+"Bank Error In Your Favor. Collect $200."
+"Income Tax Refund. Collect $20."
