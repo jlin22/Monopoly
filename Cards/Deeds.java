63537c0bf3536d08d419034be8551916c55cf7bc
Alan <azhou2@stuy.edu>
2017-01-03 14:36:53 -0500

    added get statements

--- Cards/Deeds.java
+++ Cards/Deeds.java
@@ -11,6 +11,52 @@ public class Deeds {
     private int houseCost;
     private int mortgage;
     private int unmortgage;
-    
 
+    public static int getPosition() {
+	return position;
+    }
+
+    public static String getName() {
+	return name;
+    }
+
+    public static int getRent() {
+	return rent;
+    }
+
+    public static int getRentMonopoly() {
+	return rentMonopoly;
+    }
+
+    public static int getRent1H() {
+	return rent1H;
+    }
+
+    public static int getRent2H() {
+	return rent2H;
+    }
+
+    public static int getRent3H() {
+	return rent3H;
+    }
+
+    public static int getRent4H() {
+	return rent4H;
+    }
+
+    public static int getHotel() {
+	return hotel;
+    }
+
+    public static int getHouseCost() {
+	return houseCost;
+    }
+
+    public static int getMortgage() {
+	return mortgage;
+    }
+    
+    public static int getUnmortgage() {
+	return unmortgage;
+    }
 }
