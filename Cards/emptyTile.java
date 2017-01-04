05a3709e6bb13578aa732a07ac6789bbdc3712ba
Alan <azhou2@stuy.edu>
2017-01-04 14:19:40 -0500

    constructor done

---
+++ Cards/emptyTile.java
@@ -0,0 +1,11 @@
+public class emptyTile {
+    private int position;
+    private int getMoney;
+    private int sentPosition;
+
+    public emptyTile(int a, int b,int c) {
+	a = position;
+	b = getMoney;
+	c = sentPosition;
+    }
+}
