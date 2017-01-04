f7c42585a51a4be24f926dd0152db21571e22a4c
Alan <azhou2@stuy.edu>
2017-01-03 19:20:12 -0500

    initiated first side of board

--- Monopoly.java
+++ Monopoly.java
@@ -1,2 +1,17 @@
 public class Monopoly {
+    public main static void(args[]) {
+	Deed 1 = new Deed(1,"Mediterranean Ave.",2,4,10,30,90,160,250,50,30,33);
+	Deed 2 = new Deed(2,"Baltic Ave.",4,8,20,60,180,320,450,50,30,33);
+	communityChest 3 = new communityChest(3,"not sure");
+	// 4 = income tax
+	// 5 = Reading Railroad
+	Deed 6 = new Deed(6,"Oriental Ave.",5,10,30,90,270,400,550,50,50,55);
+	Deed 7 = new Deed(7,"Vermont Ave.",5,10,30,90,270,400,550,50,50,55);
+	Chance 8 = new Chance(8,"not sure");
+	Deed 9 = new Deed(9,"Connecticut Ave.",8,16,40,100,300,450,600,50,60,66);
+	// 10 = Visiting Jail
+	Deed 11 = new Deed(11,"St. Charles Place.",);
+	// 12 = Utility
+	Deed 13 = new Deed(13,"States Ave.",);
+	Deed 14 = new Deed(14,"Virignia Ave.",);
 }
