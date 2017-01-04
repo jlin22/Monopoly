da4f1f1a5d1a0e1760362f2288ab5b86f4d8813c
Alan <azhou2@stuy.edu>
2017-01-03 20:10:07 -0500

    did some of the board setup

--- Monopoly.java
+++ Monopoly.java
@@ -1,17 +1,44 @@
 public class Monopoly {
-    public main static void(args[]) {
+    public static void main(String args[]) {
+	// 0 = GO;
 	Deed 1 = new Deed(1,"Mediterranean Ave.",2,4,10,30,90,160,250,50,30,33);
-	Deed 2 = new Deed(2,"Baltic Ave.",4,8,20,60,180,320,450,50,30,33);
-	communityChest 3 = new communityChest(3,"not sure");
+	communityChest 2 = new communityChest(2,"not sure");	
+	Deed 3 = new Deed(3,"Baltic Ave.",4,8,20,60,180,320,450,50,30,33);
 	// 4 = income tax
 	// 5 = Reading Railroad
 	Deed 6 = new Deed(6,"Oriental Ave.",5,10,30,90,270,400,550,50,50,55);
-	Deed 7 = new Deed(7,"Vermont Ave.",5,10,30,90,270,400,550,50,50,55);
-	Chance 8 = new Chance(8,"not sure");
+	Chance 7 = new Chance(7,"not sure");
+	Deed 8 = new Deed(8,"Vermont Ave.",5,10,30,90,270,400,550,50,50,55);
 	Deed 9 = new Deed(9,"Connecticut Ave.",8,16,40,100,300,450,600,50,60,66);
 	// 10 = Visiting Jail
-	Deed 11 = new Deed(11,"St. Charles Place.",);
+	Deed 11 = new Deed(11,"St. Charles Place.",10,20,50,150,450,625,750,100,70,77);
 	// 12 = Utility
-	Deed 13 = new Deed(13,"States Ave.",);
-	Deed 14 = new Deed(14,"Virignia Ave.",);
+	Deed 13 = new Deed(13,"States Ave.",10,20,50,150,450,625,750,100,70,77);
+	Deed 14 = new Deed(14,"Virginia Ave.",12,24,60,180,500,700,900,100,80,88);
+	// 15 = PA Railroad
+	Deed 16 = new Deed(16,"St. James Place",14,28,70,200,550,750,950,100,90,99);
+	communityChest 17 = new communityChest(17,"not sure");
+	Deed 18 = new Deed(18,"Tennessee Avenue",14,28,70,200,550,750,950,100,90,99);
+	Deed 19 = new Deed(19,"New York Avenue",16,32,80,220,600,800,1000,100,100,110);
+	// 20 = Free Parking;
+	Deed 21 = new Deed(21,"Kentucky Avenue",18,36,90,250,700,875,1050,150,110,121);
+	Chance 22 = new Chance(22,"");
+	Deed 23 = new Deed(23,"Indiana Avenue",18,36,90,250,700,875,1050,150,110,121); 
+	Deed 24 = new Deed(24,"Illinois Avenue",20,40,100,300,750,925,1100,150,120,132);
+	// 25 = B&O Railroad
+	Deed 26 = new Deed(26,"Atlantic Avenue",22,44,110,330,800,975,1150,150,130,143);
+	Deed 27 = new Deed(27,"Ventnor Avenue",22,44,110,330,800,975,1150,150,130,143); 
+	// 28 = Water Works
+	Deed 29 = new Deed(29,"Marvin Gardens",24,48,120,360,850,1025,1200,150,140,154);
+	// 30 = Go To Jail
+	Deed 31 = new Deed(31,"Pacific Avenue",26,52,130,390,900,1100,1275,200,150,165);
+	Deed 32 = new Deed(32,"North Carolina Avenue",26,52,130,390,900,1100,1275,200,150,165);
+	communityChest 33 = new communityChest(33,"");
+	Deed 34 = new Deed(34,"Pennsylvania Avenue",28,56,150,450,1000,1200,1400,200,160,176);
+	// 35 = Short Line
+	Chance 36 = new Chance(36,"");
+	Deed 37 = new Deed(37,"Park Place",35,70,175,500,1100,1300,1500,200,175,193);
+	// 38 = Luxury Tax
+	Deed 39 = new Deed(39,"Boardwalk",50,100,200,600,1400,1700,2000,200,200,220);
+	
 }
