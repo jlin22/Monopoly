456a9af0398e92ccb4941740792a11ea46444464
John <jlin22@stuy.edu>
2017-01-06 14:23:08 -0500

    Player class created

---
+++ Rules/Player.java
@@ -0,0 +1,9 @@
+public class Player{
+    private int money;
+    private int position;
+    public Player(){
+	money = 1500;
+	position = 0;
+    }
+    
+}
--- Rules/Rules.java
+++ Rules/Rules.java
@@ -1,3 +1,4 @@
 public class Rules {
-
+    private int turn;
+    private int playerGoing;
 }
