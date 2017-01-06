bd527d8271fdeaa507db2ca8307b2d4d306a2693
John <jlin22@stuy.edu>
2017-01-06 14:42:44 -0500

    Player and Rules functions

--- Rules/Player.java
+++ Rules/Player.java
@@ -1,17 +1,26 @@
+import java.util.*;
 public class Player{
     private int money;
     private int position;
+    private int numberRolled;
+    private ArrayList<String> propertiesOwned;
     public Player(){
 	money = 1500;
 	position = 0;
+	numberRolled = 0;
+	propertiesOwned = new ArrayList<String>();
     }
-    public addMoney(int x){
+    public void addMoney(int x){
 	money += x;
     }
-    public loseMoney(int x){
+    public void loseMoney(int x){
 	money -= x;
     }
-    public goForward(int x){
+    public void goForward(int x){
 	position += x;
+	position = position % 40;
+    }
+    public void setNumberRolled(int x){
+	numberRolled = x;
     }
 }
--- Rules/Rules.java
+++ Rules/Rules.java
@@ -1,4 +1,33 @@
 public class Rules {
     private int turn;
     private int playerGoing;
+    private int players;
+    private boolean rolledDice;
+    public Rules(){
+	turn = 1;
+	playerGoing = 1;
+	players = 4;
+	rolledDice = false;
+    }
+    public int getTurn(){
+	return turn;
+    }
+    public int getPlayerGoing(){
+	return turn / players;
+    }
+    public int getPlayers(){
+	return players;}
+    public boolean getRolledDice(){
+	return rolledDice;
+    }
+    public void rollDice(){
+	if (!rolledDice){
+	    int randomNum,randomNum1 = 0;
+	    randomNum = 1 + (int)(Math.random() * 6);
+	    randomNum1 = 1 + (int)(Math.random() * 6);
+	} 
+    }
+    public static void main(String[]a){
+    }
+	
 }
