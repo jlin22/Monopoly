9406d94c2945b473199ee9b3c824c94fdf0c145a
John <jlin22@stuy.edu>
2017-01-06 14:28:24 -0500

    functions for player

--- Rules/Player.java
+++ Rules/Player.java
@@ -5,5 +5,13 @@ public class Player{
 	money = 1500;
 	position = 0;
     }
-    
+    public addMoney(int x){
+	money += x;
+    }
+    public loseMoney(int x){
+	money -= x;
+    }
+    public goForward(int x){
+	position += x;
+    }
 }
