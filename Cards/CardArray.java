5bfdceaa8beb19cb20f916a67e9a323c8ef59fd8
John Lin <jlin22@stuy.edu>
2017-01-07 22:33:07 -0500

    finished randomized CardLists

--- Cards/CardArray.java
+++ Cards/CardArray.java
@@ -2,7 +2,13 @@ import java.util.*;
 public class CardArray{
     private ArrayList<String> communityChestList;
     private ArrayList<String> ChanceList;
+    private ArrayList<String> randomizedCommunityChest;
+    private ArrayList<String> randomizedChanceList;
     public CardArray(){
+	communityChestList= new ArrayList<String>();
+	ChanceList= new ArrayList<String>();
+	randomizedCommunityChest = new ArrayList<String>();
+	randomizedChanceList = new ArrayList<String>();
 	communityChestList.add("Collect $50 From Every Player.");
 	communityChestList.add("Receive $25 For Services.");
        	communityChestList.add("Advance To Go (Collect $200).");
@@ -32,6 +38,42 @@ public class CardArray{
 	ChanceList.add("Advance to Illinois Ave.");
 	ChanceList.add("This Card May Be Kept Until Needed or Sold. GET OUT OF JAIL FREE.");
 	ChanceList.add("Make General Repairs On All Your Property. For Each House Pay $25. For Each Hotel $100.");
-	 
-    }						 
+	createRandomizedCommunityChest();
+	createRandomizedChance();
+	
+    }
+    public void createRandomizedCommunityChest(){
+	ArrayList<String> x = new ArrayList<String>();
+	for (int i = 0; i < communityChestList.size(); i++){
+	    x.add(communityChestList.get(i));
+	}
+	    for (int r = 0; r < communityChestList.size(); r++){
+	    int y = (int)(Math.random()*(x.size()));
+	    randomizedCommunityChest.add(x.get(y));
+	    x.remove(x.get(y));
+	}	    
+    }
+       public void createRandomizedChance(){
+	ArrayList<String> x1 = new ArrayList<String>();
+	for (int i1 = 0; i1 < ChanceList.size(); i1++){
+	    x1.add(ChanceList.get(i1));
+	}
+	    for (int r1 = 0; r1 < ChanceList.size(); r1++){
+	    int y1 = (int)(Math.random()*(x1.size()));
+	    randomizedChanceList.add(x1.get(y1));
+	    x1.remove(x1.get(y1));
+	}	    
+    }
+    public ArrayList<String> getRandomizedCommunityChest(){
+	return randomizedCommunityChest;
+    }
+    public ArrayList<String> getRandomizedChance(){
+	return randomizedChanceList;
+    }
+
+    public static void main(String[]a){
+	CardArray x = new CardArray();
+	System.out.println(x.getRandomizedChance());
+	System.out.println(x.getRandomizedCommunityChest());
+    }
 }
