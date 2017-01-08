c973538c01daad63838d1bc64c9a180fe23ed171
John Lin <jlin22@stuy.edu>
2017-01-07 21:51:07 -0500

    finished communitychest

--- Cards/CardArray.java
+++ Cards/CardArray.java
@@ -1,8 +1,23 @@
 import java.util.*;
 public class CardArray{
-    private ArrayList<communityChest> communityChestList;
+    private ArrayList<String> communityChestList;
     public CardArray(){
-	communityChest cc1 = new CommunityChest(1,1,1,1,1);
+	communityChestList.add("Collect $50 From Every Player.");
+	communityChestList.add("Receive $25 For Services.");
+       	communityChestList.add("Advance To Go (Collect $200).");
+	communityChestList.add("Pay Hospital Fees. Pay $100.");
+	communityChestList.add("Doctor's Fee. Pay $50.");
+	communityChestList.add("Get Out of Jail, Free.");
+	communityChestList.add("From Sale of Stock, You Get $45.");
+	communityChestList.add("Go To Jail. Go Directly to Jail. Do Not Pass Go. Do Not collect $200.");
+	communityChestList.add("Life Insurance Matures. Collect $100.");
+	communityChestList.add("You Have Won Second Prize In A Beauty Contest. Collect $10");
+	communityChestList.add("Xmas Fund Matures. Collect $100.");
+	
+	communityChestList.add("You Are Assessed For Street Repairs. $40 Per House. $115 Per Hotel.");
+	communityChestList.add("Bank Error In Your Favor. Collect $200.");
+	communityChestList.add("Income Tax Refund. Collect $20.");
+	    
     }
 						
     
