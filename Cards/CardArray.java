9be40f815014d163f741fe858074eadc414dcffb
John Lin <jlin22@stuy.edu>
2017-01-07 21:36:25 -0500

    created cardArray

---
+++ Cards/CardArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CardArray{
+    private ArrayList<communityChest> communityChestList;
+    public CardArray(){
+	communityChest cc1 = new CommunityChest(1,1,1,1,1);
+    }
+						
+    
+}
