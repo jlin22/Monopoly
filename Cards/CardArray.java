d55bd3fc804553e99d8ad47c6b75f843ab1adc15
John Lin <jlin22@stuy.edu>
2017-01-07 21:55:21 -0500

    finished ChanceList

--- Cards/CardArray.java
+++ Cards/CardArray.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class CardArray{
     private ArrayList<String> communityChestList;
+    private ArrayList<String> ChanceList;
     public CardArray(){
 	communityChestList.add("Collect $50 From Every Player.");
 	communityChestList.add("Receive $25 For Services.");
@@ -12,13 +13,25 @@ public class CardArray{
 	communityChestList.add("Go To Jail. Go Directly to Jail. Do Not Pass Go. Do Not collect $200.");
 	communityChestList.add("Life Insurance Matures. Collect $100.");
 	communityChestList.add("You Have Won Second Prize In A Beauty Contest. Collect $10");
-	communityChestList.add("Xmas Fund Matures. Collect $100.");
-	
+	communityChestList.add("Xmas Fund Matures. Collect $100.");	
 	communityChestList.add("You Are Assessed For Street Repairs. $40 Per House. $115 Per Hotel.");
 	communityChestList.add("Bank Error In Your Favor. Collect $200.");
 	communityChestList.add("Income Tax Refund. Collect $20.");
-	    
-    }
-						
-    
+	ChanceList.add("Advance To Go. (Collect $200)");
+	ChanceList.add("Bank Pays You Dividend of $50.");
+	ChanceList.add("Go Back 3 Spaces.");
+	ChanceList.add("Advance Token to Nearest Utility. If Unowned You May Buy It From Bank. If Owned, Throw Dice and Pay Owner a Total Ten Times the Amount Thrown.");
+	ChanceList.add("Go DIRECTLY To Jail. Do Not Pass Go, Do Not Collect $200.");
+	ChanceList.add("Pay Poor Tax of $15.");
+	ChanceList.add("Advance to St. Charle's Place. If You Pass Go, Collect $200.");
+	ChanceList.add("You Have Been Elected Chairman of The Board. Pay Each Player $50.");
+	ChanceList.add("Advance Token to the Nearest Railroad and Pay Owner Twice the Rental to Which He is Otherwise Entitled. If Railroad is Unowned, You May Buy it From the Bank.");
+	ChanceList.add("Take A Ride On the Reading Railroad. If You Pass Go, Collect $200.");
+	ChanceList.add("Take a Walk on The Boardwalk. Advance Token to Boardwalk.");
+	ChanceList.add("Your Building and Loan Matures. Collect $150.");
+	ChanceList.add("Advance to Illinois Ave.");
+	ChanceList.add("This Card May Be Kept Until Needed or Sold. GET OUT OF JAIL FREE.");
+	ChanceList.add("Make General Repairs On All Your Property. For Each House Pay $25. For Each Hotel $100.");
+	 
+    }						 
 }
