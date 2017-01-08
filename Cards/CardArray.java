958f190a80bcdb0dc9c53d8ea81d1838579c5c79
Alan <azhou2@stuy.edu>
2017-01-07 23:12:42 -0500

    added a few cards

--- Cards/CardArray.java
+++ Cards/CardArray.java
@@ -9,35 +9,37 @@ public class CardArray{
 	ChanceList= new ArrayList<String>();
 	randomizedCommunityChest = new ArrayList<String>();
 	randomizedChanceList = new ArrayList<String>();
-	communityChestList.add("Collect $50 From Every Player.");
-	communityChestList.add("Receive $25 For Services.");
-       	communityChestList.add("Advance To Go (Collect $200).");
-	communityChestList.add("Pay Hospital Fees. Pay $100.");
-	communityChestList.add("Doctor's Fee. Pay $50.");
-	communityChestList.add("Get Out of Jail, Free.");
-	communityChestList.add("From Sale of Stock, You Get $45.");
-	communityChestList.add("Go To Jail. Go Directly to Jail. Do Not Pass Go. Do Not collect $200.");
-	communityChestList.add("Life Insurance Matures. Collect $100.");
-	communityChestList.add("You Have Won Second Prize In A Beauty Contest. Collect $10");
-	communityChestList.add("Xmas Fund Matures. Collect $100.");	
-	communityChestList.add("You Are Assessed For Street Repairs. $40 Per House. $115 Per Hotel.");
-	communityChestList.add("Bank Error In Your Favor. Collect $200.");
-	communityChestList.add("Income Tax Refund. Collect $20.");
-	ChanceList.add("Advance To Go. (Collect $200)");
-	ChanceList.add("Bank Pays You Dividend of $50.");
-	ChanceList.add("Go Back 3 Spaces.");
-	ChanceList.add("Advance Token to Nearest Utility. If Unowned You May Buy It From Bank. If Owned, Throw Dice and Pay Owner a Total Ten Times the Amount Thrown.");
-	ChanceList.add("Go DIRECTLY To Jail. Do Not Pass Go, Do Not Collect $200.");
-	ChanceList.add("Pay Poor Tax of $15.");
-	ChanceList.add("Advance to St. Charle's Place. If You Pass Go, Collect $200.");
-	ChanceList.add("You Have Been Elected Chairman of The Board. Pay Each Player $50.");
-	ChanceList.add("Advance Token to the Nearest Railroad and Pay Owner Twice the Rental to Which He is Otherwise Entitled. If Railroad is Unowned, You May Buy it From the Bank.");
-	ChanceList.add("Take A Ride On the Reading Railroad. If You Pass Go, Collect $200.");
-	ChanceList.add("Take a Walk on The Boardwalk. Advance Token to Boardwalk.");
-	ChanceList.add("Your Building and Loan Matures. Collect $150.");
-	ChanceList.add("Advance to Illinois Ave.");
-	ChanceList.add("This Card May Be Kept Until Needed or Sold. GET OUT OF JAIL FREE.");
-	ChanceList.add("Make General Repairs On All Your Property. For Each House Pay $25. For Each Hotel $100.");
+	communityChestList.add("It Is Your Birthday. Collect $10 From Every Player."); //
+	communityChestList.add("You Inherit $100."); 
+	communityChestList.add("Receive $25 For Consultancy Fee."); //
+	communityChestList.add("Pay Hospital Fees. Pay $100."); //
+	communityChestList.add("Doctor's Fee. Pay $50.");//
+	communityChestList.add("From Sale of Stock. You Get $50."); //
+	communityChestList.add("Go To Jail. Go Directly to Jail. Do Not Pass Go. Do Not collect $200."); //
+	communityChestList.add("Life Insurance Matures. Collect $100.");//
+	communityChestList.add("You Have Won Second Prize In A Beauty Contest. Collect $10"); //
+	communityChestList.add("Holiday Fund Matures. Collect $100."); //	
+	communityChestList.add("You Are Assessed For Street Repairs. $40 Per House. $115 Per Hotel."); //
+	communityChestList.add("Bank Error In Your Favor. Collect $200."); //
+	communityChestList.add("Income Tax Refund. Collect $20.");//
+	communityChestList.add("Get Out of Jail Free Card."); //
+	communityChestList.add("Pay School Fees of $50."); //
+	communityChestList.add("Advance To Go. (Collect $200)"); //
+	ChanceList.add("Advance To Go. (Collect $200)"); //
+	ChanceList.add("Bank Pays You Dividend of $50."); //
+	ChanceList.add("Go Back 3 Spaces.");//
+	ChanceList.add("Advance Token to Nearest Utility. If Unowned You May Buy It From Bank. If Owned, Throw Dice and Pay Owner a Total Ten Times the Amount Thrown."); //
+	ChanceList.add("Go DIRECTLY To Jail. Do Not Pass Go, Do Not Collect $200.");//
+	ChanceList.add("Speeding Fine $15."); //
+	ChanceList.add("Advance to St. Charle's Place. If You Pass Go, Collect $200.");//
+	ChanceList.add("Advance Token to the Nearest Railroad and Pay Owner Twice the Rental to Which He is Otherwise Entitled. If Railroad is Unowned, You May Buy it From the Bank."); //
+	ChanceList.add("Advance Token to the Nearest Railroad and Pay Owner Twice the Rental to Which He is Otherwise Entitled. If Railroad is Unowned, You May Buy it From the Bank."); //
+	ChanceList.add("Take A Trip To the Reading Railroad. If You Pass Go, Collect $200."); //
+	ChanceList.add("Advance to Boardwalk."); //
+	ChanceList.add("Your Building and Loan Matures. Collect $150."); //
+	ChanceList.add("Advance to Illinois Ave. (If You Pass 'GO' Collect $200.)"); //
+	ChanceList.add("This Card May Be Kept Until Needed or Sold. GET OUT OF JAIL FREE."); //
+	ChanceList.add("Make General Repairs On All Your Property. For Each House Pay $25. For Each Hotel $100."); //
 	createRandomizedCommunityChest();
 	createRandomizedChance();
 	
